e42959e3b8260682eebbbf928bcd54c2f464749c
Espoir Murhabazi <EspoirMur@users.noreply.github.com>
2023-07-08 13:16:31 +0100

    fix the encoder part

--- triton_model_repository/encoder_decoder_model/1/model.py
+++ triton_model_repository/encoder_decoder_model/1/model.py
@@ -1,10 +1,8 @@
 from typing import Dict, List
-import numpy as np
 import triton_python_backend_utils as pb_utils
 from pathlib import Path
 from optimum.onnxruntime import ORTModelForSeq2SeqLM
-from transformers import AutoConfig
-
+import torch
 
 TOKENIZER_SW_LANG_CODE_TO_ID = 128088
 
@@ -16,9 +14,8 @@ class TritonPythonModel:
         Initialize the tokenization process
         :param args: arguments from Triton config file
         """
-        current_path: str = Path(__file__).parent
-        model_path = current_path.joinpath(f"m2m100_418M_en_swa_rel_news_quantized")
-        print(f"Loading model from  the current path is {current_path} and the model path {model_path}", model_path.exists())
+        current_path: str = Path(args["model_repository"]).parent.absolute()
+        model_path = current_path.joinpath("encoder_decoder_model", "1", f"m2m100_418M_en_swa_rel_news_quantized")
         self.device = "cpu" if args["model_instance_kind"] == "CPU" else "cuda"
         # more variables in https://github.com/triton-inference-server/python_backend/blob/main/src/python.cc
         self.model = ORTModelForSeq2SeqLM.from_pretrained(model_path,
@@ -37,10 +34,17 @@ class TritonPythonModel:
         # for loop for batch requests (disabled in our case)
         for request in requests:
             # binary data typed back to string
-            query = [t.decode("UTF-8") for t in pb_utils.get_input_tensor_by_name(request, "TEXT").as_numpy().tolist()]
-            input_ids = input_ids.type(dtype=np.int32)
+            input_ids = pb_utils.get_input_tensor_by_name(request, "input_ids").as_numpy()
+            attention_masks = pb_utils.get_input_tensor_by_name(request, "attention_mask").as_numpy()
+            input_ids = torch.as_tensor(input_ids, dtype=torch.int64)
+            attention_masks = torch.as_tensor(attention_masks, dtype=torch.int64)
             if self.device == "cuda":
                 input_ids = input_ids.to("cuda")
-            generated_indices = self.model.generate(input_ids, forced_bos_token_id=TOKENIZER_SW_LANG_CODE_TO_ID)
-            responses.append(pb_utils.InferenceResponse(generated_indices))
+                attention_masks = attention_masks.to("cuda")
+            model_inputs = {"input_ids": input_ids, "attention_mask": attention_masks}
+            generated_indices = self.model.generate(**model_inputs,
+                                                    forced_bos_token_id=TOKENIZER_SW_LANG_CODE_TO_ID)
+            tensor_output = pb_utils.Tensor("generated_indices", generated_indices.numpy())
+            responses.append(tensor_output)
+        responses = [pb_utils.InferenceResponse(output_tensors=responses)]
         return responses
